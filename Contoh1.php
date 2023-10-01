111bd1e3d7d5ebe205bdbd07cc072ff8c87d2af3
fadsLAB <fadligundul890@gmail.com>
2023-10-01 20:32:29 +0700

    menampilkan biodata

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan</h2>";
+        echo"Nama Saya Fadli Nur Hidayat
+            Saya tinggal di daerah Klaten
+            Saya hobi bermain alat musik gitar
+            maupun gamelan, dan olahraga favorit 
+            saya adalah futsal";
+    }
+}
